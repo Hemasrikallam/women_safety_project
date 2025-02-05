dfc1e3834b224daa9f4a57d06a8cb5d8a44f2e35
Hemasrikallam <104370899+Hemasrikallam@users.noreply.github.com>
2025-02-05 17:23:26 +0530

    Update custom_appBar.dart

--- lib/widgets/home_widgets/custom_appBar.dart
+++ lib/widgets/home_widgets/custom_appBar.dart
@@ -1,24 +1,83 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:women_safety/utils/quotes.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:animate_do/animate_do.dart';
 
-class CustomAppBar extends StatelessWidget {
-  // const CustomAppBar({super.key});
-  Function? onTap;
-  int? quoteIndex;
-  CustomAppBar({this.onTap, this.quoteIndex});
+class QuoteCard extends StatefulWidget {
+  final int? quoteIndex;
+  final VoidCallback? onTap;
+
+  QuoteCard({this.quoteIndex, this.onTap});
+
+  @override
+  _QuoteCardState createState() => _QuoteCardState();
+}
+
+class _QuoteCardState extends State<QuoteCard> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    )..repeat(reverse: true);
+    _fadeAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        onTap!();
-      },
-      child: Container(
-        child: Text(
-          sweetSayings[quoteIndex!],
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold
+    return BounceInDown(
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFFD8080), Color(0xFFFBD079)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(24),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black38,
+                blurRadius: 8,
+                offset: Offset(3, 5),
+              ),
+            ],
+          ),
+          child: FadeTransition(
+            opacity: _fadeAnimation,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.format_quote, color: Colors.white, size: 24),
+                SizedBox(width: 8),
+                Expanded(
+                  child: Text(
+                    sweetSayings[widget.quoteIndex!],
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ),
+                Icon(Icons.format_quote, color: Colors.white, size: 24),
+              ],
+            ),
           ),
         ),
       ),
